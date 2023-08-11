b9277e68a2d5cceb82954cf12ce39a59e4302544
DESKTOP-70UHL5T\OM <mk525156@gmail.com>
2023-08-11 23:14:38 +0530

    - class&xml

---
+++ app/src/free/java/com/example/videoinfo/FreeActivity.kt
@@ -0,0 +1,11 @@
+package com.example.videoinfo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FreeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_free)
+    }
+}
